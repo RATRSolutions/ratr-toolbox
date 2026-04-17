5f3c0db96bb5652ec4650fa345f9517df4e32d40
Tom Nordgård <tomnordgard@gmail.com>
2026-04-17 17:08:26 +0200

    feat: strip app down to RFLP Viewer only for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import MdToDocx from './pages/MdToDocx';
-import Laeringsplan from './pages/Laeringsplan';
 import RflpViewer from './pages/RflpViewer';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/md-to-docx" element={<MdToDocx />} />
-        <Route path="/laeringsplan" element={<Laeringsplan />} />
-        <Route path="/rflp-viewer" element={<RflpViewer />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RflpViewer />;
 }
 
 export default App;
